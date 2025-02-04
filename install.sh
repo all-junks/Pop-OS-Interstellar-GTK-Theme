6607348f707e28c618d148a1750b4d3eca9fa21a
Ishan-Git <88433029+all-junks@users.noreply.github.com>
2025-02-04 13:05:44 -0500

    Update v1.0.4

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/all-junks/Frapoppe.git; cd Frapoppe; mv Frapoppe-theme.zip Pop-OS-Interstellar-Theme/; cd ..; unzip -q '*.zip'; rm -f *.zip; sudo mv Icons/Zafiro-Nord-Black-Blue /usr/share/icons/; sudo mv Fonts/JetBrainsMono-2.304 /usr/share/fonts/; sudo mv Cursors/Catppuccin-Frappe-Light-Cursors /usr/share/icons/; cd Frapoppe-theme; ./install.sh
+git clone https://github.com/all-junks/Frapoppe.git; unzip -q '*.zip'; rm -f *.zip; sudo mv Icons/Zafiro-Nord-Black-Blue /usr/share/icons/; sudo mv Fonts/JetBrainsMono-2.304 /usr/share/fonts/; sudo mv Cursors/Catppuccin-Frappe-Light-Cursors /usr/share/icons/; cd Frapoppe-theme; chmod 755 install.sh; chmod +x install.sh; ./install.sh
 
 PS3="Select your distro: "
 
-select opt in deb arch rhel solus suse
+select opt in deb arch rhel suse
 do
     case $opt in
         "deb")
@@ -19,10 +19,6 @@ do
         "rhel")
             sudo dnf install plank plank-docklets
             sudo dnf update
-            break;;
-        "solus")
-            sudo eopkg install plank
-            sudo eopkg upgrade
             break;;
         "suse")
             sudo zypper install plank
