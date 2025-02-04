ce85da31733562c1be8f87642007f98670f42938
Ishan-Git <88433029+all-junks@users.noreply.github.com>
2025-02-04 12:57:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/all-junks/Frapoppe.git; cd Frapoppe; mv Frapoppe-theme.zip Pop-OS-Interstellar-Theme/; cd ..; unzip -q '*.zip'; rm -f *.zip; sudo mv Icons/Zafiro-Nord-Black-Blue /usr/share/icons/; sudo mv Fonts/JetBrainsMono-2.304 /usr/share/fonts/; sudo mv Cursors/Catppuccin-Frappe-Light-Cursors /usr/share/icons/; cd Frapoppe-theme; chmod 755 install.sh; chmod +x install.sh; ./install.sh
+git clone https://github.com/all-junks/Frapoppe.git; cd Frapoppe; mv Frapoppe-theme.zip Pop-OS-Interstellar-Theme/; cd ..; unzip -q '*.zip'; rm -f *.zip; sudo mv Icons/Zafiro-Nord-Black-Blue /usr/share/icons/; sudo mv Fonts/JetBrainsMono-2.304 /usr/share/fonts/; sudo mv Cursors/Catppuccin-Frappe-Light-Cursors /usr/share/icons/; cd Frapoppe-theme; ./install.sh
 
 PS3="Select your distro: "
 
