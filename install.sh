b8711486725406c61cc1bab56cc6d9d9cf7dd23d
Ishan-Git <88433029+all-junks@users.noreply.github.com>
2025-02-05 16:00:34 +0000

    Update v1.0.6

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/all-junks/Frapoppe.git; unzip -q '*.zip'; rm -f *.zip; sudo cp -r Icons/Zafiro-Nord-Black-Blue /usr/share/icons/; sudo cp -r Fonts/JetBrainsMono-2.304 /usr/share/fonts/; sudo cp -r Cursors/Catppuccin-Frappe-Light-Cursors /usr/share/icons/; cd Frapoppe; chmod 755 install.sh; chmod +x install.sh; ./install.sh
 
-PS3="Select your distro: "
+PS3="Select your distro for installation of Plank dock: "
 
 select opt in deb arch rhel suse
 do
