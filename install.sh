637334fdec371f54a18321861b9849a577d0bf22
Ishan-Git <88433029+all-junks@users.noreply.github.com>
2025-02-07 22:27:09 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git clone https://github.com/all-junks/Frapoppe.git; git clone https://github.com/catppuccin/plank.git; unzip -q '*.zip'; rm -f *.zip; sudo cp -r Icons/Zafiro-Nord-Black-Blue /usr/share/icons/; sudo cp -r Fonts/JetBrainsMono-2.304 /usr/share/fonts/; sudo cp -r Cursors/Catppuccin-Frappe-Light-Cursors /usr/share/icons/; cd Frapoppe; chmod 755 install.sh; chmod +x install.sh; ./install.sh; cd ..; cd plank/src/
+
+PS3="Select your distro for installation of Plank dock: "
+
+select opt in deb arch rhel suse
+do
+    case $opt in
+        "deb")
+            sudo apt install plank
+            sudo apt update
+            sudo apt upgrade
+            break;;
+        "arch")
+            sudo pacman -S plank
+            sudo pacman -Syu
+            break;;
+        "rhel")
+            sudo dnf install plank plank-docklets
+            sudo dnf update
+            break;;
+        "suse")
+            sudo zypper install plank
+            sudo zypper refresh
+            sudo zypper update
+            break;;
+        *)
+            echo Invalid option, retrying... ;;
+    esac
+done
+
+plank; sudo cp Catppuccin-frappe ~/.local/share/plank/themes/
